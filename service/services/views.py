d36b9272ba2fa0549ca3b654b219aea92f205c57
Steadypim <ilyagt75@gmail.com>
2023-02-01 14:51:32 +1000

    Optimize ORM query 2, use only to select needed fields, add plan in prefetch_related to select related plans

--- service/services/views.py
+++ service/services/views.py
@@ -1,5 +1,4 @@
 from django.db.models import Prefetch
-from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -9,7 +8,8 @@ from services.serializers import SubscriptionSerializer
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
-                                                              'user__email'))
+                                                                                     'user__email'))
     )
     serializer_class = SubscriptionSerializer
